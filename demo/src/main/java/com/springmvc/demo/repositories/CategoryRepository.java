79c7d0bd0d82538770c36b0ea0901ee481adf83b
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-11-13 22:41:22 +0700

    add @Repository

--- demo/src/main/java/com/springmvc/demo/repositories/CategoryRepository.java
+++ demo/src/main/java/com/springmvc/demo/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.springmvc.demo.repositories;
 
 import com.springmvc.demo.models.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, String> {
 }
--- demo/src/main/java/com/springmvc/demo/repositories/ProductRepository.java
+++ demo/src/main/java/com/springmvc/demo/repositories/ProductRepository.java
@@ -3,7 +3,9 @@ package com.springmvc.demo.repositories;
 import com.springmvc.demo.models.Category;
 import com.springmvc.demo.models.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, String> {
     Iterable<Product> findByCategoryID(String categoryID);
 }
